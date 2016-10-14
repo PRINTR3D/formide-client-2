2539e356439bdc1f17baca463e961879886dcd8f
Chris ter Beke <ctbeke@gmail.com>
2016-10-14 17:36:29 +0200

    add katana module and dependency

--- src/slicer/index.js
+++ src/slicer/index.js
@@ -5,10 +5,37 @@
  *	Copyright (c) 2015, All rights reserved, http://printr.nl
  */
 
+const Globals = require('../core/globals');
+
 class Slicer {
 
     constructor() {
 
+        try {
+            this.katana = require('katana-slicer');
+            this.reference = require('katana-slicer/reference');
+            Globals.log(`Loaded Katana v${this.reference.version}`, 1, 'info');
+        }
+        catch (e) {
+            console.log(e);
+            Globals.log('Cannot load Katana binary, try re-installing katana-slicer', 1, 'warn');
+        }
+    }
+
+    slice() {
+
+    }
+
+    createSliceRequest() {
+
+    }
+
+    /**
+     * Get reference file for Katana slice engine
+     * @returns {*}
+     */
+    getReferenceFile() {
+        return this.reference;
     }
 }
 
