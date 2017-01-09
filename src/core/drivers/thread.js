2fb8d146162f2ddb88f62762a54c0687bc743b7e
Chris ter Beke <ctbeke@gmail.com>
2017-01-09 18:01:11 +0100

    fork timing

--- src/core/drivers/thread.js
+++ src/core/drivers/thread.js
@@ -3,7 +3,7 @@
 * @Date:   2016-12-18T17:09:26+01:00
 * @Filename: thread.js
 * @Last modified by:   chris
-* @Last modified time: 2017-01-07T16:43:16+01:00
+* @Last modified time: 2017-01-09T18:00:49+01:00
 * @Copyright: Copyright (c) 2016, All rights reserved, http://printr.nl
 */
 
@@ -17,6 +17,8 @@
 
 let driver = false
 
+console.time('driverFork')
+
 try {
   driver = require('formide-drivers')
 } catch (e) {
@@ -28,6 +30,7 @@ if (driver) {
     if (err) {
       process.send({type: 'error', data: err})
     } else if (started) {
+      console.timeEnd('driverFork')
       process.send({type: 'started', data: started})
     } else if (event) {
       process.send({type: 'event', data: event}) // every time an event comes from the drivers we pass it to the main thread
