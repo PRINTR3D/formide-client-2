3eb11b0dbfd65700b04c6e09e814c253d81ad8d1
ChrisTerBeke <chris@printr.nl>
2017-02-21 00:12:40 +0100

    test for websocket incorrect jwt auth

--- test/tests/websocket/index.test.js
+++ test/tests/websocket/index.test.js
@@ -6,10 +6,10 @@ const jwt = require('../../../src/core/utils/jwt')
 const io = require('socket.io-client')
 
 module.exports = (client) => {
-	describe('Auth', () => {
-		describe('POST /api/auth/login', () => {
+	describe('WebSockets', () => {
+		describe('Authenticate', () => {
 			
-			it('should authenticate using a correct JWT token', (done) => {
+			it('should authenticate when using a correct JWT token', (done) => {
 				const user = client.auth.find('admin@local')
 				const token = jwt.sign(user)
 				const socketClient = io.connect(`http://localhost:${client.config.http.api}`)
@@ -19,6 +19,19 @@ module.exports = (client) => {
 				}, (data) => {
 					expect(data.success).to.equal(true)
 					expect(data.user.id).to.equal(user.id)
+					socketClient.disconnect()
+					done()
+				})
+			})
+			
+			it('should not authenticate when using an incorrect JWT', (done) => {
+				const socketClient = io.connect(`http://localhost:${client.config.http.api}`)
+				
+				socketClient.emit('authenticate', {
+					token: 'wrongtoken'
+				}, (data) => {
+					expect(data.success).to.equal(false)
+					socketClient.disconnect()
 					done()
 				})
 			})
