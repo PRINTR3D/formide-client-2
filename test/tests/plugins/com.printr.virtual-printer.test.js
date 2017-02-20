488258399f8af9e4f44f9e197916e70d7e791cfd
ChrisTerBeke <chris@printr.nl>
2017-02-20 23:36:24 +0100

    more test

--- test/tests/plugins/com.printr.virtual-printer.test.js
+++ test/tests/plugins/com.printr.virtual-printer.test.js
@@ -9,6 +9,16 @@ module.exports = (client) => {
 	describe('Virtual Printer', () => {
 		describe('GET /plugins/com.printr.virtual-printer/api/status', () => {
 			
+			it('should set the status of the virtual printer', (done) => {
+				chai.request(client.http.app).get('/plugins/com.printr.virtual-printer/api/status').query({
+					status: 'online'
+				}).end((req, res) => {
+					expect(res.status).to.equal(200)
+					// TODO: check actual virtual printer status after 2 seconds
+					done()
+				})
+			})
+			
 			it('should throw a 400 when the username parameter is missing', (done) => {
 				chai.request(client.http.app).get('/plugins/com.printr.virtual-printer/api/status').send({}).end((req, res) => {
 					expect(res.status).to.equal(400)
