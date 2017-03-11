64584bafaee9de596787336fa2b007ca89e05546
ChrisTerBeke <chris@printr.com>
2017-03-11 21:39:20 +0100

    cleanup

--- test/index.test.js
+++ test/index.test.js
@@ -7,19 +7,18 @@ client.plugins.loadPlugin(`${__dirname}/plugins/com.printr.virtual-printer`)
 
 // manually load tests to prevent wrong order and inject client instance
 // core
-// require('./tests/core/auth.test')(client)
-// require('./tests/core/drivers/printerCommandTemplate.test')(client)
+require('./tests/core/auth.test')(client)
+require('./tests/core/drivers/printerCommandTemplate.test')(client)
 
 // routes
-// require('./tests/routes/auth.test')(client)
+require('./tests/routes/auth.test')(client)
 // require('./tests/routes/network.test')(client) // TODO: network list test is slow
-// require('./tests/routes/printer.test')(client)
-// require('./tests/routes/storage.test')(client)
-// require('./tests/routes/update.test')(client)
+require('./tests/routes/printer.test')(client)
+require('./tests/routes/storage.test')(client)
+require('./tests/routes/update.test')(client)
 
 // websockets
-// require('./tests/websocket/index.test')(client)
+require('./tests/websocket/index.test')(client)
 
 // cloud
 require('./tests/cloud/cloudServer.test')(client)
-// require('./tests/cloud/downloadGcodeFromCloud.test')(client) // TODO: slow test
