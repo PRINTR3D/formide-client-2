614f5a3a61fc4853cff5f8eda492f34c4e9bc937
ChrisTerBeke <chris@printr.com>
2017-03-28 17:03:06 +0200

    more debug log

--- src/core/cloud/getCloudQueue.js
+++ src/core/cloud/getCloudQueue.js
@@ -9,6 +9,7 @@ const request = require('request')
  * @returns {Promise}
  */
 function getCloudQueue (client, port) {
+	console.log('getCloudQueue: client.cloud.getDeviceToken()', client.cloud.getDeviceToken())
 	return new Promise((resolve, reject) => {
 		request.get(`${client.config.cloud.URL}/db/queue/device/port/${encodeURIComponent(port)}`, {
 			auth: {
