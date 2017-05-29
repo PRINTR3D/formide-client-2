172014c6b07a02691a2642d7f3dffdf410ba20ae
ChrisTerBeke <chris@printr.nl>
2017-05-29 22:00:23 +0200

    cleanup file

--- src/core/utils/logo.js
+++ src/core/utils/logo.js
@@ -1,12 +1,3 @@
-/**
-* @Author: chris
-* @Date:   2016-12-18T17:20:10+01:00
-* @Filename: logo.js
-* @Last modified by:   chris
-* @Last modified time: 2016-12-30T14:34:17+01:00
-* @Copyright: Copyright (c) 2016, All rights reserved, http://printr.nl
-*/
-
 'use strict'
 
 function logLogo (config) {
